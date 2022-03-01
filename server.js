8d20a63ef6abd295e9557129976d224df47f7c13
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-02-28 16:54:13 -0800

    built server, browser routes

--- server.js
+++ server.js
@@ -3,16 +3,31 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
+const Book = require('./models/book');
 const app = express();
 app.use(cors());
+const PORT = process.env.PORT || 3002;
 
-const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+app.get('/', (request, response) => {
+  response.status(200).send('Greetings from server');
+});
 
-  response.send('test request received')
+app.get('*', (request, response) => {
+  response.status(404).send('This aint it');
+});
 
-})
+
+
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
